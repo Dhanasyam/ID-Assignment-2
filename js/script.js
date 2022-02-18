52497d6834bcb0c97384d93aa44955be8849968b
OngWeiLun <S10222794@connect.np.edu.sg>
2022-02-18 19:36:59 +0800

    Created functions for the quiz; Buttons to log a number when clicked on; Quiz now shows the respective question when loaded; Not able to log score or check answer yet;

--- js/script.js
+++ js/script.js
@@ -4,24 +4,66 @@ $(document).ready(function(){
 
 var questionList = []
 
+function loadQuiz(onSuccess){
+
+}
+
 $.ajax({
     type: "GET",
     dataType: "json",
     contentType: "text/plain",
-    url: "https://quizapi.io/api/v1/questions?apiKey=Z2oZ44G44xVOWQeKf8dXuY9JvK2IMfG7acVBoPw3&category=linux&difficulty=Easy&limit=10&tags=Linux",
+    url: "https://quizapi.io/api/v1/questions?apiKey=Z2oZ44G44xVOWQeKf8dXuY9JvK2IMfG7acVBoPw3&category=linux&difficulty=Easy&tags=Linux&multiple_correct_answers=false",
     headers: {
 
     },
     success: function(data){
-        for(let i = 0; i < 10; i ++){
-            questionList[i] = data[i];
+        let numCollect = 0;
+        for (let i = 0; i < data.length; i++){
+            if(questionList.length == 10){
+                break;
+            }
+            let curr = data[i];
+            var count = countProperties(curr.answers);
+            if(count === 4){
+                questionList.push(curr);
+            }
+            
         }
-        console.log(questionList);
+        printingQuestion(questionList);
     }
 });
 
+function countProperties(obj) {
+    let count = 0;
+    for(let prop in obj) {
+        if(obj.hasOwnProperty(prop) && obj[prop]) {
+          ++count;
+        }
+
+    }
+
+    return count;
+}
+
+
+
 function printingQuestion(questionList){
-    $()
+
+    q = questionList[0];
+    
+    $("#questionTitle").text(q.question);
+
+    console.log(q.question)
+
+    $("#c1").text(q.answers.answer_a);
+    $("#c2").text(q.answers.answer_b);
+    $("#c3").text(q.answers.answer_c);
+    $("#c4").text(q.answers.answer_d);
+    
+}
+
+function checkAnswer(number){
+    console.log(number);
 }
 
 
