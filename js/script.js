e3dd3e88fa462c58d91dbe9b50c1a97b25cbf60e
OngWeiLun <S10222794@connect.np.edu.sg>
2022-02-19 06:36:34 +0800

    Quiz now working; End of quiz not finished;

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ var questionList = []
 function loadQuiz(onSuccess){
 
 }
-
 $.ajax({
     type: "GET",
     dataType: "json",
@@ -17,7 +16,9 @@ $.ajax({
 
     },
     success: function(data){
-        let numCollect = 0;
+        
+        var numofQn = 0;
+        console.log(data);
         for (let i = 0; i < data.length; i++){
             if(questionList.length == 10){
                 break;
@@ -29,7 +30,17 @@ $.ajax({
             }
             
         }
-        printingQuestion(questionList);
+
+        // for (let t = 0; t < questionList.length;t++){
+
+        //     $("#progressText").text("Question " + (t+1));
+        //     question = questionList[t];
+        //     printingQuestion(question);
+            
+        // }
+        console.log(questionList);
+        q = questionList[numofQn];
+        printingQuestion(q,numofQn);
     }
 });
 
@@ -39,7 +50,6 @@ function countProperties(obj) {
         if(obj.hasOwnProperty(prop) && obj[prop]) {
           ++count;
         }
-
     }
 
     return count;
@@ -47,9 +57,9 @@ function countProperties(obj) {
 
 
 
-function printingQuestion(questionList){
+function printingQuestion(q,numofQn){
 
-    q = questionList[0];
+    $("#progressText").text("Question " + (numofQn+1));
     
     $("#questionTitle").text(q.question);
 
@@ -59,12 +69,87 @@ function printingQuestion(questionList){
     $("#c2").text(q.answers.answer_b);
     $("#c3").text(q.answers.answer_c);
     $("#c4").text(q.answers.answer_d);
+
+    numofQn++
     
 }
 
-function checkAnswer(number){
-    console.log(number);
+
+
+function returnAnswer(number,questionList){
+
+    var scores = Number($("#score").text());
+    var chk = checkAnswer(number,questionList[0]);
+    if(chk == true){
+        scores += 1;
+    }
+    console.log("Score: " + scores);
+    $("#score").text(scores);
+    questionList.shift()
+    if(questionList.length === 0 ){
+        console.log(scores);
+        endOfQuiz()
+        return;
+    }
+    var n = 10 - questionList.length
+    var q1 = questionList[0];
+    printingQuestion(q1,n)
 }
 
+function checkAnswer(n,qchk){
+    console.log("chkAns" + qchk.question);
+    var userCharCode = getCharCode(n);
+    console.log("user" + userCharCode);
+    var ans = getAnswer(qchk.correct_answers);
+    var qnCharCode = getCharCode(ans);
+    console.log("answer" + qnCharCode);
 
+    if(userCharCode === qnCharCode){
+        console.log("Correct!");
+        return true;
+    }
+    else{
+        console.log("Wrong!");
+        return false;
+    }
 
+}
+
+function getAnswer(obj){
+    
+    let count = 0;
+    for(let prop in obj) {
+        console.log(obj[prop])
+        ++count
+        if(obj[prop] === "true") {
+          break;
+        }
+    }
+    return count;
+}
+
+function getCharCode(n){
+    if(n === 1){
+        charCode = 97;
+    }
+    else if (n === 2){
+        charCode = 98;
+    }
+    else if (n === 3){
+        charCode = 99;
+    }
+    else{
+        charCode = 100;
+    }
+    return charCode;
+}
+
+function endOfQuiz(){
+    var finalScore = Number($("#score").text())
+    $(".hud-prefix").hide()
+    $("#score").hide();
+    $("#questionTitle").html("<h1>Quiz finished!<br>Final Score: "
+    + finalScore + "</h1>");
+    $(".choice-container").hide();
+    $("#progressText").hide()
+}
