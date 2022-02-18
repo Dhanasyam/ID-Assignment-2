cbb5c22e07dfce44700c4ded941f5aeac32db39b
OngWeiLun <S10222794@connect.np.edu.sg>
2022-02-18 12:24:18 +0800

    removed unnecassary script tags on all html docs; added js file for leaderboard; started working on leaderboard GET;

---
+++ js/leaderboard.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    const APIKEY = "620f118234fd62156585877b";
+
+    function getScores(limit = 10, all = true){
+    
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://quizwebsiteasg2-c11c.restdb.io/rest/leaderboard",
+            "method": "GET",
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+            }
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+
+        let content = "";
+
+        for (var i = 0; i < response.length && i < limit; i++){
+            
+        }
+    })
+
+});
