335271581061c16d9c05e806ee76f94063e59655
Thaikkoottathil Dhanasyam <92707155+Dhanasyam@users.noreply.github.com>
2022-02-20 01:21:47 +0800

    Changes to leaderboeard  and script

--- js/leaderboard.js
+++ js/leaderboard.js
@@ -2,31 +2,80 @@ $(document).ready(function(){
 
     const APIKEY = "620f118234fd62156585877b";
 
+    $("input#name-submit").on("click", function(e){
+
+        e.preventDefault();
+        var timeElapsed = Date.now();
+        var today = new Date(timeElapsed);
+        let Name = $("#username").val();
+        let Score = Number($("#score").text());
+        let quizDate = today.getDate() + "-" + (today.getMonth()+1) + "-" + today.getFullYear();
+    
+        let jsondataleaderboard = {
+            "Username":Name,
+            "Score": Score,
+            "Date": quizDate
+        }
+    
+        console.log(jsondataleaderboard);
+    
+        set={
+            "async": true,
+            "crossDomain": true,
+            "url": "https://quizwebsiteasg2-c11c.restdb.io/rest/leaderboard",
+            "method": "POST",
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": "620f118234fd62156585877b",
+                "cache-control": "no-cache"
+            },
+            "processData": false,
+            "data": JSON.stringify(jsondataleaderboard)
+        }
+
+        $.ajax(set).done(function (response) {
+            console.log(response);
+            
+            $("#contact-submit").prop( "disabled", false);
+
+            getScores();
+          });
+    
+        window.location.href = "index.html";
+    
+    })
+
 
 
 });
-function getScores(limit = 10, all = true){
 
-    var settings = {
+function getScores(limit = 10, all = true) {
+
+    let set = {
         "async": true,
         "crossDomain": true,
         "url": "https://quizwebsiteasg2-c11c.restdb.io/rest/leaderboard",
         "method": "GET",
         "headers": {
             "content-type": "application/json",
-            "x-apikey": APIKEY,
+            "x-apikey": "620f118234fd62156585877b",
             "cache-control": "no-cache"
-        }
+        },
     }
 
-    $.ajax(settings).done(function (response) {
-    console.log(response);
+    $.ajax(set).done(function (response) {
 
-    let content = "";
+      let content = "";
 
-    for (var i = 0; i < response.length && i < limit; i++){
-        
-        }
-    })
+      for (var i = 0; i < response.length && i < limit; i++) {
+
+        console.log(response);
+
+        content = `${content}<tr id='${i + 1}'><td>${response[i].Username}</td>
+        <td>${response[i].Score}</td><td>${response[i].Date}</tr>`;
+
+      }
+      $("#Leaderboard-list tbody").html(content);
+    });
 }
 
--- js/script.js
+++ js/script.js
@@ -153,7 +153,6 @@ function endOfQuiz(){
 
 }
 
-//Leaderboard data
 $("input#name-submit").on("click", function(e){
 
     e.preventDefault();
@@ -185,40 +184,9 @@ $("input#name-submit").on("click", function(e){
         "data": JSON.stringify(jsondataleaderboard)
     })
 
-    
-
     window.location.href = "index.html";
 
 })
-
-function getContacts(limit = 10, all = true) {
-
-    let set = {
-        "async": true,
-        "crossDomain": true,
-        "url": "https://quizwebsiteasg2-c11c.restdb.io/rest/leaderboard",
-        "method": "POST",
-        "headers": {
-            "content-type": "application/json",
-            "x-apikey": "620f118234fd62156585877b",
-            "cache-control": "no-cache"
-        },
-    }
-    
-    $.ajax(set).done(function (response) {
-      
-      let content = "";
-
-      for (var i = 0; i < response.length && i < limit; i++) {
-        
-        
-        content = `${content}<tr id='${i + 1}'><td>${response[i].Username}</td>
-        <td>${response[i].Score}</td>
-        <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-msg='${response[i].message}' data-name='${response[i].name}' data-email='${response[i].email}'>Update</a></td></tr>`;
-
-      }
-      $("#Leaderboard-list tbody").html(content);
-    });
 
 // function submitFunction(){
 //     console.log("Submitted");
@@ -244,4 +212,3 @@ function getContacts(limit = 10, all = true) {
     // console.log(response);
     // });
 // }
-}
