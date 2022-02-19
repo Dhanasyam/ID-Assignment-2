e3adb4bef1695825db6346e28d884c8b480784b2
Thaikkoottathil Dhanasyam <92707155+Dhanasyam@users.noreply.github.com>
2022-02-19 23:11:15 +0800

    Made improvement to the leaderboard part

--- js/script.js
+++ js/script.js
@@ -153,6 +153,7 @@ function endOfQuiz(){
 
 }
 
+//Leaderboard data
 $("input#name-submit").on("click", function(e){
 
     e.preventDefault();
@@ -184,9 +185,40 @@ $("input#name-submit").on("click", function(e){
         "data": JSON.stringify(jsondataleaderboard)
     })
 
+    
+
     window.location.href = "index.html";
 
 })
+
+function getContacts(limit = 10, all = true) {
+
+    let set = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://quizwebsiteasg2-c11c.restdb.io/rest/leaderboard",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json",
+            "x-apikey": "620f118234fd62156585877b",
+            "cache-control": "no-cache"
+        },
+    }
+    
+    $.ajax(set).done(function (response) {
+      
+      let content = "";
+
+      for (var i = 0; i < response.length && i < limit; i++) {
+        
+        
+        content = `${content}<tr id='${i + 1}'><td>${response[i].Username}</td>
+        <td>${response[i].Score}</td>
+        <td><a href='#' class='delete' data-id='${response[i]._id}'>Del</a></td><td><a href='#update-contact-container' class='update' data-id='${response[i]._id}' data-msg='${response[i].message}' data-name='${response[i].name}' data-email='${response[i].email}'>Update</a></td></tr>`;
+
+      }
+      $("#Leaderboard-list tbody").html(content);
+    });
 
 // function submitFunction(){
 //     console.log("Submitted");
@@ -212,4 +244,4 @@ $("input#name-submit").on("click", function(e){
     // console.log(response);
     // });
 // }
-
+}
